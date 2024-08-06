c12da31495106893b49d08e2bb1c14480573bb02
Jakob Runge <jakob.runge@gridfuse.com>
2024-08-06 11:09:24 +0200

    confirm that sha256 and crypto.subtle.digest are compatible

--- src/sha2/sha256.test.ts
+++ src/sha2/sha256.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { bench, expect, test } from "vitest";
 import { sha256, SHA256 } from "./sha256.js";
 
 test("SHA256", async () => {
@@ -10,5 +10,17 @@ test("SHA256", async () => {
 			hash.update(randomValues.slice(j * 5, (j + 1) * 5));
 		}
 		expect(hash.digest()).toStrictEqual(expected);
+	}
+});
+
+test("sha256 behaves like SubtleCrypto digest", async () => {
+	const randomValues = crypto.getRandomValues(new Uint8Array(5 * 100));
+	for (let i = 0; i < randomValues.byteLength / 5; i++) {
+		const example = randomValues.slice(0, i * 5);
+
+		const expected = new Uint8Array(await crypto.subtle.digest("SHA-256", example));
+		const actual = sha256(example);
+
+		expect(actual).toStrictEqual(expected);
 	}
 });
